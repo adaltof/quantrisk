17ee26ee41b892b2f0f4106fc80a4082c9ed4ea2
adaltof <adaltof@gmail.com>
2020-12-22 23:33:36 -0500

    Initial build up of mockup app

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, jsonify
-from models import User
+from models import User, ThreatCommunity
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def initial_page():
+    # Get Total Users
+    usercount = User.objects.count()
+    threatcount = ThreatCommunity.objects.count()
+    return render_template('index.html', usercount=usercount, threatcount=threatcount)
 
-@app.route('/test')
-def template_test():
-    return render_template('template.html', my_string='Teste 123')
 
-@app.route('/users')
+@app.route('/user')
 def list_users():
     users = User.objects()
     users_print = []
@@ -23,6 +23,15 @@ def list_users():
     print(users)
 
     return render_template('users.html', users=users_print)
+
+@app.route('/user/<user>')
+def user_page(user):
+    user = User.objects(name=user).first()
+    return render_template('user.html', user=user)
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('page-404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -17,6 +17,40 @@ class User(db.Document):
     name = db.StringField(required=True)
     email = db.StringField()
 
+class ThreatCommunity(db.Document):
+    name = db.StringField(required=True)
+    description = db.StringField(required=False)
+    tcapmax = db.IntField(min_value=0, max_value=100)
+    tcapmin = db.IntField(min_value=0, max_value=100)
+    tcapavg = db.IntField(min_value=0, max_value=100)
+
+class AssetTarget(db.Document):
+    name = db.StringField(required=True)
+    description = db.StringField(required=False)
+    rsmax = db.IntField(min_value=0, max_value=100)
+    rsmin = db.IntField(min_value=0, max_value=100)
+    rsavg = db.IntField(min_value=0, max_value=100)
+
+class Analysis(db.Document):
+    name = db.StringField(required=True)
+    description = db.StringField(required=False)
+    datecreated = db.DateTimeField()
+    lefmax = db.DecimalField(min_value=0, precision=2)
+    lefmin = db.DecimalField(min_value=0, precision=2)
+    lefavg = db.DecimalField(min_value=0, precision=2)
+    tefmax = db.DecimalField(min_value=0, precision=2)
+    tefmin = db.DecimalField(min_value=0, precision=2)
+    tefavg = db.DecimalField(min_value=0, precision=2)
+    cfmax = db.DecimalField(min_value=0, precision=2)
+    cfmin = db.DecimalField(min_value=0, precision=2)
+    cfavg = db.DecimalField(min_value=0, precision=2)
+    tcapmax = db.IntField(min_value=0, max_value=100)
+    tcapmin = db.IntField(min_value=0, max_value=100)
+    tcapavg = db.IntField(min_value=0, max_value=100)
+    rsmax = db.IntField(min_value=0, max_value=100)
+    rsmin = db.IntField(min_value=0, max_value=100)
+    rsavg = db.IntField(min_value=0, max_value=100)
+
 
 if __name__ == 'main':
     app.run(debug=True)
